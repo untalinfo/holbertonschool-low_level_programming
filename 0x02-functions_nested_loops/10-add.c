d030b2553e18cc0a1921fcdb8bb47173934d5200
untalinfo <racso1607@gmail.com>
2020-02-11 20:19:30 +0000

    ejercice with funtion, add tow ints and return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - function that adds tow integres and return
+ * the result
+ * @x: integer 1
+ * @y: integer 2
+ * Return: result int.
+ */
+
+int add(int x, int y)
+{
+	int r;
+
+	r = x + y;
+	return (r);
+}
