e4754310a87371ce2407d5b69de5f334d548b204
untalinfo <racso1607@gmail.com>
2020-02-14 03:03:05 +0000

    practice funtions and more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-  * print_square - function that prints a square.
+  * print_triangle - function that prints a triangle.
   * @size: size number of square
   * Return: None.
   */
