73e0fdcc995964be7c6f5f6a11e54bf5eea22e75
untalinfo <racso1607@gmail.com>
2020-02-18 15:25:59 +0000

    firts commit, in exercise 0, asig the new valor for one variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * as parameter and updates the value it points to to 98
+ *
+ * Return:
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
