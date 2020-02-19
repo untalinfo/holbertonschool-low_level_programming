0027d82c96051a731b438cf6fb556ffd1f3f33d3
untalinfo <racso1607@gmail.com>
2020-02-19 01:47:13 +0000

    copy the string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies the string
+ * @dest: destiny of the copy
+ * @src: source
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
