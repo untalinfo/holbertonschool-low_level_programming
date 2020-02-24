45c93834d1bb380baa2c0aa226b79791dcc52b30
untalinfo <racso1607@gmail.com>
2020-02-24 19:00:52 +0000

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: pointer to string
+ * @c: character to be locate
+ * 
+ * Return: pointer to the memory area dest
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
