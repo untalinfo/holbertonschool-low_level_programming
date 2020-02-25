90db44382c53976eaedbbfe4abc6836758c577b2
untalinfo <racso1607@gmail.com>
2020-02-25 03:52:37 +0000

    prints the sum of the tow diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers.
+ *@a: pointer to an array of arrays
+ *@size: size of square matrix
+ *Return: none
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < size; i++)
+		sum =
+	printf("%d, ", sum);
+	sum = 0;
+	for (i = 0; i < size; i++)
+		sum =
+	printf("%d\n", sum);
+}
