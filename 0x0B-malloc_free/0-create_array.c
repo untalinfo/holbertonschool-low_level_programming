b9520490936e022563023da29ec765b852c48c61
untalinfo <racso1607@gmail.com>
2020-03-03 16:54:03 +0000

    firts commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - prints buffer in hexa
+ * @size: the size of the memory to print
+ * @size: character inicial
+ *
+ * Return: array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i = 0;
+
+	if (size <= 0)
+		return (NULL);
+
+	p = malloc(sizeof(char) * size);
+	while (i < size)
+	{
+		*(p + i ) = c;
+		i++;
+	}
+	return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+##MALLOC - FREE##
+What is the difference between automatic and dynamic allocation
+What is malloc and free and how to use them
+Why and when use malloc
+IHow to use valgrind to check for memory leak
