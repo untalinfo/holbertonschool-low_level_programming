406690466329044ad3100d93381c348cadd30ba7
untalinfo <racso1607@gmail.com>
2020-03-04 04:38:14 +0000

    task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Alloc Grid
+ * @width: width to alloc
+ * @height: height to alloc
+ *
+ * Return: Grid Allocated
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, k;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	p =;
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}
