23e82a242f2e4c21fac5aa9d23fcb53ee6cb0675
untalinfo <racso1607@gmail.com>
2020-03-04 21:29:21 +0000

    task function that frees a 2 dimensional grid previously created by your alloc_grid function.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * alloc_grid - Alloc Grid
  * @width: width to alloc
@@ -14,8 +15,24 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p =;
+	p = malloc(height * sizeof(int *));
 	if (p == NULL)
 		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(width * sizeof(int));
+		if (p[i] == NULL)
+		{
+			while (i >= 0)
+				free(p[i--]);
+			free(p);
+			return (0);
+		}
+	}
+	for (j = 0; j < height; j++)
+	{
+		for (k = 0; k < width; k++)
+			p[j][k] = 0;
+	}
 	return (p);
 }
--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -1,4 +1,4 @@
-nclude "holberton.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
