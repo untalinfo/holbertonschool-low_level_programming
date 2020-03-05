8362f81b53261d9bd8b3106ec2e94880816e7add
untalinfo <racso1607@gmail.com>
2020-03-05 20:59:39 +0000

    firts commit for task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmenb: number of memory spaces
+ * @size: size of nmenb
+ * Return: pointer to allocate spaces or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *p;
+
+	if (nmemb <= 0)
+		 return (NULL);
+	if (size <= 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (; i < (nmemb * size); i++)
+		*(p + i) = 0;
+	return (p);
+}
