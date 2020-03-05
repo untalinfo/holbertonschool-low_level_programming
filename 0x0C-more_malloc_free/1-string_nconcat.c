acbc1c77b86d1e44e455f6f3f39aa57d3aeb5e6b
untalinfo <racso1607@gmail.com>
2020-03-05 20:16:48 +0000

    correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else if (n == 0)
 		j = 0;
 
-	p = malloc((sizeof(char) * i + sizeof(char) * j + 1));
+	p = malloc((sizeof(char) * i + sizeof(char) * n + 1));
 	if (p == NULL)
 		return (NULL);
 	for (; a <= i; a++)
