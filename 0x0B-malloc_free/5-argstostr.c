ecc8fec9b1dc6ae16f4b32a893b0961fd55e2b9e
untalinfo <racso1607@gmail.com>
2020-03-05 02:12:05 +0000

    function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -2,37 +2,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * alloc_grid - Alloc Grid
- * @width: width to alloc
- * @height: height to alloc
+ * argstostr - concatenates arguments of a program.
+ * @ac: argument count
+ * @av: argument values
  *
- * Return: Grid Allocated
+ * Return: pointer to string
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k;
-	int **p;
+	int i, j, len, bufferlen;
+	char *p;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	p = malloc(height * sizeof(int *));
+	i = j = len = bufferlen = 0;
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+	}
+	p = (char *)malloc(len * sizeof(char) + ac + 1);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < height; i++)
+	for (i = 0; av[i]; i++)
 	{
-		p[i] = malloc(width * sizeof(int));
-		if (p[i] == NULL)
-		{
-			while (i >= 0)
-				free(p[i--]);
-			free(p);
-			return (0);
-		}
+		for (j = 0; av[i][j]; j++, bufferlen++)
+			p[bufferlen] = av[i][j];
+		p[bufferlen] = '\n';
+		bufferlen++;
 	}
-	for (j = 0; j < height; j++)
-	{
-		for (k = 0; k < width; k++)
-			p[j][k] = 0;
-	}
+	p[bufferlen] = '\0';
 	return (p);
 }
