b7be50665d28fbb56f9ef0eaddfdf952b222ea23
untalinfo <racso1607@gmail.com>
2020-03-11 22:51:50 +0000

    firts commit for function that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that performs simple operations.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int);
+	int num1, num2;
+
+	if (argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	f = get_op_func(argv[2]);
+	if (f == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	printf("%d\n", f(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,27 @@
+int r;
+
+int op_add(int a, int b)
+{
+	r = a + b;
+	return (r);
+}
+int op_sub(int a, int b)
+{
+	r = a - b;
+	return (r);
+}
+int op_mul(int a, int b)
+{
+	r = a * b;
+	return (r);
+}
+int op_div(int a, int b)
+{
+	r = a / b;
+	return (r);
+}
+int op_mod(int a, int b)
+{
+	r = a % b;
+	return (r);
+}
