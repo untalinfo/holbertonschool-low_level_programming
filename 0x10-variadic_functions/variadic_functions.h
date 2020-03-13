d5675fd2e801470d93b799900de31e89cdcd62c8
untalinfo <racso1607@gmail.com>
2020-03-13 23:13:25 +0000

    header of the all functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,19 @@
+#ifndef V_FUNCTIONS_H
+#define V_FUNCTIONS_H
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list);
+} op_t;
+#endif
