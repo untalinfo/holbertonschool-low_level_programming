3ce5ad9fa0065b155ad5c7c152362cf3af9cfad7
untalinfo <racso1607@gmail.com>
2020-03-19 19:50:04 +0000

    fix in the files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 /**
  * print_list - print all the elements of a list_t list
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,4 +19,5 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif
