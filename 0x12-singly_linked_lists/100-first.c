54aa917b6cddcadc7077be5e977a2801bccfe6f2
untalinfo <racso1607@gmail.com>
2020-03-20 17:49:47 +0000

    function to execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * firts - function that prints a message
+ *
+ * Return: Always 0.
+ */
+void __attribute__ ((constructor)) firts()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
