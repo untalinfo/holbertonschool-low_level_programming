580d4bac3aadd6ec58347c873346fbda9463ab65
untalinfo <racso1607@gmail.com>
2020-03-24 21:02:39 +0000

    task 9 fucntion that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,36 +1,40 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index- function that inserts a new node at a given position
+ * insert_nodeint_at_index- function that inserts a
+ * new node at a given position
  * @head: pointer to list
  * @idx: index where the new node should be added.
- * n: int to store in node
+ * @n: int to store in node
  * Return: Adddress of new node, NULL if failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *tmpnod, *new;
+	unsigned int i = 0;
+	listint_t *node, *tmpnode, *aux;
 
-	if (head == NULL)
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
 		return (NULL);
-	if (*head == NULL)
+	node->n = n;
+	tmpnode = *head;
+	if (idx == 0)
 	{
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
-		*head = new;
-		new->next = NULL;
-		new->n = n;
-		return (new);
+		node->next = tmpnode;
+		*head = node;
+		return (node);
 	}
-
-	sum = 0;
-	tmpnode = head;
-
-	while (tmpnode != NULL)
+	while (tmpnode)
 	{
-		sum = sum + tmpnode->n;
+		if (i == (idx - 1))
+		{
+			aux = tmpnode->next;
+			node->next = aux;
+			tmpnode->next = node;
+			return (node);
+		}
+		i++;
 		tmpnode = tmpnode->next;
 	}
-	return (sum);
+	return (NULL);
 }
+
