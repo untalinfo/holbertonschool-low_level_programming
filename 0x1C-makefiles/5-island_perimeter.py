6e107df7b70ba57f7ca600a8629db04d43b0b66d
untalinfo <racso1607@gmail.com>
2020-07-06 02:49:52 +0000

    pep8 style in task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,7 +14,7 @@ def island_perimeter(grid):
         [Int]: [perimeter]
     """
     perimeter = 0
-    
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
